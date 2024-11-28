292191b8156eac7b3862b5989ced2143bdfbcc77
ReyliCruz <167218112+ReyliCruz@users.noreply.github.com>
2024-11-27 22:58:11 -0600

    api key disabled

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,9 +145,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'api.authentication.GlobalAPIKeyAuthentication',
-    ],
+    #'DEFAULT_AUTHENTICATION_CLASSES': [
+    #    'api.authentication.GlobalAPIKeyAuthentication',
+    #],
 }
 
 # PAPALOTE API KEY
